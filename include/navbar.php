db9011045d6e75fe093e54f2e93ec379238e01a0
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-24 16:48:29 +0800

    uptimised logo for navbar

--- include/navbar.php
+++ include/navbar.php
@@ -12,7 +12,7 @@
                 <i class="ti-search"></i>
             </a>
             <a href="index.html">
-                <img class="img-fluid" src="assets/images/logo.png" alt="Theme-Logo">
+                <img class="img-fluid" src="assets/images/uptimised.png" alt="Theme-Logo">
             </a>
             <a class="mobile-options">
                 <i class="ti-more"></i>
