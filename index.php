6173d4c9c0c833191d323113c8b34819778014f9
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-24 16:33:19 +0800

    includes re fix

--- index.php
+++ index.php
@@ -6,46 +6,46 @@
     <div class="pcoded-inner-content">
         <!-- Main-body start -->
         <div class="main-body">
-            <div class="page-wrapper">
-              <!-- Page header start -->
-              <div class="page-header m-t-10">
-                  <div class="page-header-title">
-                      <h4>Horizontal Fixed Layout</h4>
-                  </div>
-                  <div class="page-header-breadcrumb">
-                      <ul class="breadcrumb-title">
-                          <li class="breadcrumb-item">
-                              <a href="index.html">
-                                  <i class="icofont icofont-home"></i>
-                              </a>
-                          </li>
-                          <li class="breadcrumb-item"><a href="#!">Page Layouts</a>
-                          </li>
-                          <li class="breadcrumb-item"><a href="#!">Horizontal</a>
-                          </li>
-                          <li class="breadcrumb-item"><a href="#!">Fixed Layout</a>
-                          </li>
-                      </ul>
-                  </div>
+          <div class="page-wrapper">
+            <!-- Page header start -->
+            <div class="page-header m-t-10">
+              <div class="page-header-title">
+                <h4>Horizontal Fixed Layout</h4>
               </div>
-              <!-- Page header end -->
+              <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                  <li class="breadcrumb-item">
+                    <a href="index.html">
+                      <i class="icofont icofont-home"></i>
+                    </a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#!">Page Layouts</a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#!">Horizontal</a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#!">Fixed Layout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- Page header end -->
 
-              <!-- Page body start -->
-              <div class="page-body">
-                <div class="row">
-                  <div class="col-lg-12">
-                    <!-- Default card start -->
-                    <div class="card">
-                        <div class="card-block">
-                            <span>
-                                Horizontal Fixed layout is useful for those users who wants header menu options on top.
-                            </span>
-                        </div>
+            <!-- Page body start -->
+            <div class="page-body">
+              <div class="row">
+                <div class="col-lg-12">
+                  <!-- Default card start -->
+                  <div class="card">
+                    <div class="card-block">
+                      <span>
+                        Horizontal Fixed layout is useful for those users who wants header menu options on top.
+                      </span>
                     </div>
                   </div>
-                  <!-- Default card end -->
                 </div>
+                <!-- Default card end -->
               </div>
+            </div>
           </div>
           <!-- Page body end -->
         </div>
