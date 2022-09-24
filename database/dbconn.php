53529ee7706bfc0f10f710d27bfe5940a0894ee6
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-24 14:36:24 +0800

    DB CONNECTION

---
+++ database/dbconn.php
@@ -0,0 +1,17 @@
+<?php
+  $host = 'localhost'; #online hostname
+  $user = 'root'; #online username
+  $pass = ''; #online password
+  $dbms = 'uptiph'; #online dbname
+
+  try
+  {
+    $connection = new PDO("mysql:host=$host;dbname=$dbms", $user, $pass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo 'Database Connected Successfully';
+  }
+  catch (PDOException $x)
+  {
+    echo 'Database Error Connection: ' . $x->getMessage();
+  }
+?>
